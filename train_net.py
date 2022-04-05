ee2e9f14f750c0c4b4e0bf268da73ea5c2102c7b
saidinesh pola <81302966+saidineshpola@users.noreply.github.com>
2022-04-05 21:54:43 +0530

    Update train_net.py

--- train_net.py
+++ train_net.py
@@ -267,6 +267,7 @@ def setup(args):
     cfg.MODEL.ROI_HEADS.NUM_CLASSES = 323
     cfg.SOLVER.STEPS = (10000,)
     cfg.TEST.EVAL_PERIOD = 10000
+    cfg.MODEL.WEIGHTS='base_m2f.pkl'
     cfg.freeze()
     default_setup(cfg, args)
     # Setup logger for "mask_former" module
